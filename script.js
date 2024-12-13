346fde97e0a1210c0f18368d016b1d3038825a50
Siddhesh Ghag <147908529+Slayer-X164@users.noreply.github.com>
2024-12-13 22:01:57 +0530

    added preloader

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-const circle = document.querySelector(".circle");
+const cursorAnimation = () => {
+  const circle = document.querySelector(".circle");
 
-// Listen for mouse movement
-window.addEventListener("mousemove", (e) => {
-  let x = e.clientX
-  let y = e.clientY
+  window.addEventListener("mousemove", (e) => {
+    let x = e.clientX;
+    let y = e.clientY;
 
-  circle.style.left = x - 14 + "px"
-  circle.style.top = y - 16+ "px"
+    circle.style.left = x - 14 + "px";
+    circle.style.top = y - 16 + "px";
 
-  circle.animate({
-    left:x - 14 + "px",
-    top:y - 16+ "px"
-  },{duration:1,fill:"forwards"})
-});
+    circle.animate(
+      {
+        left: x - 14 + "px",
+        top: y - 16 + "px",
+      },
+      { duration: 1, fill: "forwards" }
+    );
+  });
+};
+
+const preLoader = ()=>{
+    const loaderContainer = document.querySelector(".loader_container")
+    const main = document.querySelector("main")
+
+    window.addEventListener("load",()=>{
+        loaderContainer.classList.add('hidden')
+        main.classList.add('visible')
+    })
+}
+preLoader()
+cursorAnimation();
